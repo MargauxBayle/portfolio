0f4f60c37c2e275e19cfe14f83009d32ce2e971b
MargauxBayle <margauxba98@gmail.com>
2025-09-03 15:46:39 +0200

    add loader

--- js/style.js
+++ js/style.js
@@ -4,4 +4,26 @@ document.querySelectorAll("label ul a").forEach((link) => {
   });
 });
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.to(".dot", {
+  y: -60,
+  stagger: {
+    each: 0.2,
+    repeat: -1,
+    yoyo: true,
+  },
+});
+
+gsap.to(".shadow", {
+  y: 60,
+  stagger: {
+    each: 0.2,
+    repeat: -1,
+    yoyo: true,
+  },
+  opacity: 0.1,
+});
+
+const loader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  loader.style.display = "none";
+});
